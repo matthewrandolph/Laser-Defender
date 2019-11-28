7e3cdcf55fdddf44837126a595e56d977a5b89c7
Matthew Randolph <mrandolph@css.edu>
2019-11-27 21:53:54 -0600

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [Header("Enemy Stats")]
+    [SerializeField] int scoreValue = 150;
+    [SerializeField] float health = 100;
+
+    [Header("Shooting")]
+    float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 5f;
+
+    [Header("Sound Effects")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] AudioClip deathSound;
+    [Range(0f, 1f)] [SerializeField] float shootSoundVolume = 0.25f;
+    [Range(0f, 1f)] [SerializeField] float deathSoundVolume = 0.75f;
+
+    // Use this for initialization
+    void Start () {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountDownAndShoot();
+	}
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+            projectile,
+            transform.position,
+            Quaternion.identity
+            ) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        AudioSource.PlayClipAtPoint(shootSound, transform.position, shootSoundVolume);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
+    }
+}
---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    [SerializeField] Text scoreText;
+    [SerializeField] GameSession gameSession;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GetComponent<Text>();
+        gameSession = FindObjectOfType<GameSession>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = gameSession.GetScore().ToString();
+	}
+}
